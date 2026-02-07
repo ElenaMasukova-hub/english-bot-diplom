58f245243ce5cd11bd86f8863eb7ece85445ac11
Elena Masukova <masukova1992@gmail.com>
2026-02-07 20:45:37 +0300

    Add config template

---
+++ config_template.py
@@ -0,0 +1,9 @@
+﻿# config_template.py - скопируй в config.py и заполни свои данные
+TOKEN = 'ТВОЙ_ТОКЕН_ОТ_BOTFATHER'
+
+DB_CONFIG = {
+    'dbname': 'english_bot',
+    'user': 'postgres', 
+    'password': 'postgres',
+    'host': 'localhost'
+}
